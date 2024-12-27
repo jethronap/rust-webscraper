4c1a0d84139843b9df42e65938ed2d034e9851a8
jethronap <efthioa@gmail.com>
2024-12-27 17:30:01 +0200

    Set up basic scraper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use scraper::{Html, Selector};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let url = "https://www.rust-lang.org";
+
+    let response = reqwest::get(url).await?.text().await?;
+    println!("Fetched document from {}", url);
+
+    let document = Html::parse_document(&response);
+
+    let selector = Selector::parse("a").unwrap();  // Select all anchor tags
+
+    for element in document.select(&selector) {
+        if let Some(link) = element.value().attr("href") {
+            println!("Found link: {}", link);
+        }
+    }
+
+    Ok(())
+
 }
